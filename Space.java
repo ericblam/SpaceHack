41f5520b2e812844b5c83913848886cc508a4d98
Eric Lam <captainpicardlam@gmail.com>
2012-06-14 18:01:24 -0400

    Add Space and Upgrade.

---
+++ Space.java
@@ -0,0 +1,10 @@
+public class Space extends MapNode {
+  
+  public static final int SPACE_DEFENSE = 1;
+  public static final int SPACE_TRACTION = 3;
+  
+  public Space(Level g, int x, int y) {
+    super(g, x, y, "Space", SPACE_DEFENSE, SPACE_TRACTION);
+  }
+  
+}
---
+++ Upgrade.java
@@ -0,0 +1,10 @@
+public abstract class Upgrade extends Item {
+ 
+  private int b;
+  
+  public Upgrade(int weight, int bonus) {
+    super(weight);
+    b = bonus;
+  }
+  
+}
